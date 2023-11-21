eba949b6bc1b11795389a3184975f668e78dc7e6
root <root@ip-172-31-35-218.ap-south-1.compute.internal>
2023-11-21 07:24:29 +0000

    Another example of Nested IF statement

---
+++ conditions/nested_if_2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Another Example of nested_if statement
+
+echo "Please enter the value for i:"
+read num
+if [[ $num -ge 100 ]]
+then
+        echo "Entered Number is Higher Than 100"
+	if (( $num % 2 == 0 ))
+        then
+           echo "It is also Even number"
+        else
+           echo "It is an Odd Number"
+        fi
+else 
+	echo " Entered Number is Less than 100"
+fi
