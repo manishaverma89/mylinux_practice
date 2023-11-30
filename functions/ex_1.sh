5132556c6c850c23fe4d0795ff5d06a11da08142
manishaverma89 <manishaverma89@github.com>
2023-11-30 04:45:43 +0000

    Understanding the concept of functions

---
+++ functions/ex_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function examplefun( ) 
+
+{
+  echo "Hi my name is $1 and I am learning $2"
+
+}
+
+examplefun Manisha DevOps
+
+
+
+function showParams(){
+
+
+  echo "I am learning $2 now, then I would like to learn $1"
+
+}
+
+showParams Linux Unix
