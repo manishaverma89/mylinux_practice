634e8db9598568cf79a6e199d4865ed15c883616
manishaverma89 <manishaverma89@gmail.com>
2023-12-01 07:01:37 +0000

    Checking the permissions of file whether file is writable or not

--- assignment/2_filePath_exist.sh
+++ assignment/2_filePath_exist.sh
@@ -9,6 +9,22 @@ read -p "Enter the file name:  "  filename
 if [ -e $filename ]
 then
     echo "file_path passwords are enabled"
+    echo " "
+    echo "Now check permissions of file"
+    echo " " 
+    ls -la $filename
+
+    
+    if [ -w $filename ]
+    then
+	    echo "You have permissions to edit the file"
+    else
+	    echo "You do not have permissions to edit the file"
+    fi
+
 else
     echo "file_path passwords are not enabled"
 fi
+
+
+
